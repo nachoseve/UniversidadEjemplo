83ee04269bbf5c27bbe07eb83f3f50243aa2b044
nachoseve <141686969+nachoseve@users.noreply.github.com>
2023-09-10 16:37:38 -0300

    Escribi codigo

--- src/universidadejemplo/UniversidadEjemplo.java
+++ src/universidadejemplo/UniversidadEjemplo.java
@@ -15,6 +15,7 @@ public class UniversidadEjemplo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         MenuPrincipal mp = new MenuPrincipal();
         mp.setVisible(true);
     }
